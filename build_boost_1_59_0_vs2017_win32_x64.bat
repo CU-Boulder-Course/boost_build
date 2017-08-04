8a59fdfd5da7d2c72b3cc31eacd342b1312f308a
AdamPI <shirong.bai@colorado.edu>
2017-08-04 13:07:38 -0600

    boost 1_59_0

--- build_boost_1_59_0_vs2017_win32_x64.bat
+++ build_boost_1_59_0_vs2017_win32_x64.bat
@@ -1,13 +1,17 @@
 @ECHO OFF
 SETLOCAL
 
-:: time measurement start
+REM time measurement start
 set start=%time%
 
 ECHO ###############################################################################
 ECHO ########################## -BUILDING BJAM BINARIES- ###########################
 ECHO ###############################################################################
 SET MYPATH="%CD%"
+
+REM There are different letters you can use like f for "full path name", d for drive letter, p for path, 
+REM and they can be combined. %~ is the beginning for each of those sequences and a number I denotes it 
+REM works on the parameter %I (where %0 is the complete name of the batch file, just like you assumed)
 cd "%~dp0"
 if exist .\b2.exe del .\b2.exe
 if exist .\bjam.exe del .\bjam.exe
@@ -38,7 +42,7 @@ if exist .\stage_x64 rmdir .\stage_x64 /s/q
 
 CD %MYPATH%
 
-:: time measurement end
+REM time measurement end
 set end=%time%
 set options="tokens=1-4 delims=:.,"
 for /f %options% %%a in ("%start%") do set start_h=%%a&set /a start_m=100%%b %% 100&set /a start_s=100%%c %% 100&set /a start_ms=100%%d %% 100
