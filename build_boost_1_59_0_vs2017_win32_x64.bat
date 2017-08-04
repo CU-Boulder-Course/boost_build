cce9e88c294f572cf627da3e4056b7884db20595
AdamPI <shirong.bai@colorado.edu>
2017-08-04 17:44:59 -0600

    Keep compiled file under same directory.

--- build_boost_1_59_0_vs2017_win32_x64.bat
+++ build_boost_1_59_0_vs2017_win32_x64.bat
@@ -25,25 +25,18 @@ ECHO ###########################################################################
 ECHO ########## -BUILDING BOOST LIBRARIES 32bit **RUNTIME-LINK = ALL**- ############
 ECHO ###############################################################################
 if exist .\stage_x86 rmdir .\stage_x86 /s/q
-b2.exe --toolset=msvc-14.1 --clean-all
 call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86
+b2.exe --toolset=msvc-14.1 --clean-all
 b2.exe --toolset=msvc-14.1 architecture=x86 address-model=32 --stagedir=".\stage_x86" threading=multi --build-type=complete stage
-if not exist ..\bin\x86 md ..\bin\x86
-move /y .\stage_x86\lib\*.* ..\bin\x86
 
 if exist .\bin.v2 rmdir .\bin.v2 /s/q
 ECHO ###############################################################################
 ECHO ########## -BUILDING BOOST LIBRARIES 64bit **RUNTIME-LINK = ALL**- ############
 ECHO ###############################################################################
 if exist .\stage_x64 rmdir \stage_x64 /s/q
-b2.exe --toolset=msvc-14.1 --clean-all
 call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x64
+b2.exe --toolset=msvc-14.1 --clean-all
 b2.exe --toolset=msvc-14.1 architecture=x86 address-model=64 --stagedir=".\stage_x64" threading=multi --build-type=complete stage
-if not exist .\stage md .\stage
-if not exist ..\bin\x64 md ..\bin\x64
-move /y .\stage_x64\lib\*.* ..\bin\x64
-if exist .\stage_x86 rmdir .\stage_x86 /s/q
-if exist .\stage_x64 rmdir .\stage_x64 /s/q
 
 CD %MYPATH%
 
